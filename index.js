49749a70e7bc5d8991e022885c3a6e54fe5a1d89
Luisvilas <luisvboas@outlook.com>
2025-11-07 16:49:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 /**** Escreva o código abaixo 👇******/
+function somar(a,b){
+    return a + b
+}
 
+somar(2,3)
 
+function multiplicar(a,b){
+    return a * b
+}
 
+multiplicar(2,3)
 
+function subtrair(a,b){
+    return a - b
+}
 
+subtrair(5,3)
 
+function dividir(a,b){
+  if (b === 0){
+    return "Error"
+  }else{
+    return a / b
+  }
+    
+}
 
+dividir(6,3)
+
+function calcularMedia(a,b,c,d){
+  const media= (a + b + c + d)/4
+  return media
+}
+
+calcularMedia(7,8,9,10)
 
 /**** Escreva o código acima ☝️******/
 
